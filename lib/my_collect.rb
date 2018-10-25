a09ed37009ab0e25dfb19b28ea8717d73f786067
Jane Venter <janeanneventer@gmail.com>
2018-10-25 18:07:47 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ my_collect(collection)
   counter = 0 
   new_collection = []
   while counter < collection.length 
-    new_collection << yield(collection[i])
+    new_collection << yield(collection[counter])
     counter+=1 
   end
   new_collection
