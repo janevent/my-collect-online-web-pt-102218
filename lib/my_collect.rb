48c0823680c7a9edb8ab8b48ddfcc902c56924e1
Jane Venter <janeanneventer@gmail.com>
2018-10-25 17:58:46 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+my_collect(collection)
+  counter = 0 
+  new_collection = []
+  while counter < collection.length 
+    new_collection << yield(collection[i])
+    counter+=1 
+  end
+  new_collection
+end
 
